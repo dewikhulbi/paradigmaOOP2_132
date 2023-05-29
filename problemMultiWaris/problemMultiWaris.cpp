89092391d3c1dc5e25dce4bcc7f15de0e71e7f36
dewikhulbi <dewikhlb20@gmail.com>
2023-05-29 10:42:05 +0700

    membuat class pekerja

--- problemMultiWaris/problemMultiWaris.cpp
+++ problemMultiWaris/problemMultiWaris.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class orang {
-public:
+class orang{
+	public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+	}
 };
+
+class pekerja: 
